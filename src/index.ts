9b3910f73d7a802dd92cad386445ce56ce73adf6
Zahid <zahidmohammad495@gmai.com>
2024-03-12 09:25:37 +0530

    create table in database

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { Client } from "pg";
+
+const client = new Client({
+  connectionString:
+    "postgresql://zahid-404:0tgz2eRhuSaQ@ep-rough-surf-86840070.us-east-2.aws.neon.tech/test?sslmode=require",
+});
+
+
+
+async function createUsersTable() {
+  await client.connect();
+  const result = await client.query(`
+  CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+`);
+  console.log(result);
+}
+
+createUsersTable();
